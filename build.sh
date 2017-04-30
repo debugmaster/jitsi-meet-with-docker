bf99641acb695d409707b4e1cc24c6349de461f9
Camilo <camilo@camilo.fm>
2017-04-30 20:13:09 -0300

    Fix errors while building Prosody

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ source project.env
 docker build \
     --build-arg RELEASE=$PROSODY_RELEASE \
     --build-arg REPOSITORY=$PROSODY_REPOSITORY \
+    --build-arg SHOULD_BUILD=$BUILD_PROSODY \
     -t prosody ./docker/prosody/
