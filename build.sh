acc4390019dda24f85889222ed4575fcb2bcf4bb
Camilo <camilo@camilo.fm>
2017-05-27 12:33:52 -0300

    Add Dockerfile for Jicofo. Install Jicofo from APT or build it.

--- build.sh
+++ build.sh
@@ -12,3 +12,11 @@ docker build \
     --build-arg SHOULD_BUILD=$BUILD_PROSODY \
     -t $DOCKER_REPOSITORY/prosody \
     -f ./docker/prosody/Dockerfile-prosody ./docker/prosody/
+
+docker build \
+    --build-arg RELEASE=$JICOFO_RELEASE \
+    --build-arg RELEASE_VERSION=$JICOFO_RELEASE_VERSION \
+    --build-arg REPOSITORY=$JICOFO_REPOSITORY \
+    --build-arg SHOULD_BUILD=$BUILD_JICOFO \
+    -t $DOCKER_REPOSITORY/jicofo \
+    -f ./docker/jitsi/Dockerfile-jicofo ./docker/jitsi/
