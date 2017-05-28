b81f0e38d3a728ffb318d18e860c7a2439b377d1
Camilo <camilo@camilo.fm>
2017-05-28 12:15:57 -0300

    Add Dockerfile for Jitsi Videobridge.

--- build.sh
+++ build.sh
@@ -20,3 +20,11 @@ docker build \
     --build-arg SHOULD_BUILD=$BUILD_JICOFO \
     -t $DOCKER_REPOSITORY/jicofo \
     -f ./docker/jitsi/Dockerfile-jicofo ./docker/jitsi/
+
+docker build \
+    --build-arg RELEASE=$JVB_RELEASE \
+    --build-arg RELEASE_VERSION=$JVB_RELEASE_VERSION \
+    --build-arg REPOSITORY=$JVB_REPOSITORY \
+    --build-arg SHOULD_BUILD=$BUILD_JVB \
+    -t $DOCKER_REPOSITORY/jitsi-videobridge \
+    -f ./docker/jitsi/Dockerfile-jitsi-videobridge ./docker/jitsi/
