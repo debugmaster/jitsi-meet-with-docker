62733b9e3bab8dff881123ef7258ac5a4420e10b
Camilo <camilo@camilo.fm>
2017-07-05 09:41:32 -0300

    Run bootstrap container.

--- run.sh
+++ run.sh
@@ -4,6 +4,29 @@ set -a
 source project.env
 set +a
 
+Randomize_Port () {
+    mod=$(( $2 - $1 + 1 ));
+    start=$1;
+    while true; do
+        port=$(( ( RANDOM % $mod ) + $start ));
+        nc -z $ADVERTISED_ADDRESS $port
+        echo "b $mod $start $port $?" 1>&2;
+        nc -z $ADVERTISED_ADDRESS $port;
+        if [ $? -eq 1 ]; then
+            echo $port;
+            break;
+        fi
+    done
+}
+
+docker stop $(docker ps -q)
+
+if [[ $RUN_BOOTSTRAP == true ]]; then
+    docker-compose -f ./docker/compose/bootstrap.yml up -d
+fi
+
 if [[ $RUN_PROSODY == true ]]; then
-    docker stack deploy -c ./docker/compose/prosody.yml prosody
+    export RANDOM_RPC_PORT=$(Randomize_Port $RANDOM_PORT_MIN $RANDOM_PORT_MAX);
+    export RANDOM_LAN_PORT=$(Randomize_Port $RANDOM_PORT_MIN $RANDOM_PORT_MAX);
+    docker-compose -f ./docker/compose/prosody.yml up -d
 fi
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p /tmp/consul
+# Advertise a port different from than the one being used
+echo -e '{
+    "advertise_addrs": {
+        "rpc": "'$ADVERTISED_ADDRESS':'$RPC_PORT'",
+        "serf_lan": "'$ADVERTISED_ADDRESS':'$LAN_PORT'"
+    }
+}' > /etc/consul/consul.json
+
+consul agent -server -bootstrap -config-file=/etc/consul/consul.json \
+    -node=bootstrap \
+    -bind=$(getent hosts $HOSTNAME | awk '{ print $1 }') \
+    -data-dir=/tmp/consul
