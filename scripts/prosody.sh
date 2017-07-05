45c4f74f03cf3af305780d4b290b4879472f07ba
Camilo <camilo@camilo.fm>
2017-07-05 01:53:51 -0300

    Add Prosody config to Consul KV. Generate focus user's credentials in Prosody.

--- scripts/prosody.sh
+++ scripts/prosody.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir /tmp/consul
+mkdir -p /tmp/consul
 
 consul agent -server -bootstrap -node=focus \
     -bind=$(getent hosts $HOSTNAME | awk '{ print $1 }') \
@@ -11,8 +11,21 @@ consul agent -server -bootstrap -node=focus \
 
 sleep 3
 
+JICOFO_SECRET=$(echo $RANDOM | md5sum | awk '{ print $1 }')
+JICOFO_AUTH_USER=focus
+JICOFO_AUTH_PASSWORD=$(echo $RANDOM | md5sum | awk '{ print $1 }')
+
+prosodyctl register $JICOFO_AUTH_USER auth.$HOST $JICOFO_AUTH_PASSWORD
+
 consul kv put "config/host" $HOST
-consul kv put "component/focus/secret" secret
+consul kv put "config/ports/prosody/component" $PROSODY_COMPONENT_SERVICE_PORT
+consul kv put "config/ports/prosody/http" $PROSODY_HTTP_SERVICE_PORT
+consul kv put "config/ports/prosody/https" $PROSODY_HTTPS_SERVICE_PORT
+consul kv put "config/ports/prosody/c2s" $PROSODY_C2S_SERVICE_PORT
+consul kv put "config/ports/prosody/s2s" $PROSODY_S2S_SERVICE_PORT
+consul kv put "component/focus/secret" $JICOFO_SECRET
+consul kv put "component/focus/auth/user" $JICOFO_AUTH_USER
+consul kv put "component/focus/auth/password" $JICOFO_AUTH_PASSWORD
 
 consul-template \
     -template "/etc/prosody/templates/prosody.cfg.lua:/etc/prosody/prosody.cfg.lua:prosodyctl restart" \
