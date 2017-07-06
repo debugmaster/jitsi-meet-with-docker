1b7407ff2a218f6a3fb04ec2dff4740330cb4f01
Camilo <camilo@camilo.fm>
2017-07-06 02:46:27 -0300

    Remove unused component and rebase some code.

--- config/prosody/templates/config.cfg.lua
+++ config/prosody/templates/config.cfg.lua
@@ -6,35 +6,31 @@ admins = {
 }
 
 VirtualHost "{{ key "config/hostname" }}"
-        -- enabled = false -- Remove this line to enable this host
-        authentication = "anonymous"
-        -- Properties below are modified by jitsi-meet-tokens package config
-        -- and authentication above is switched to "token"
-        --app_id="app_id"
-        --app_secret="app_secret"
-        -- Assign this host a certificate for TLS, otherwise it would use the one
-        -- set in the global section (if any).
-        -- Note that old-style SSL on port 5223 only supports one certificate, and will always
-        -- use the global one.
-        ssl = {
-            key = "/etc/prosody/certs/localhost.key";
-            certificate = "/etc/prosody/certs/localhost.crt";
-        }
-        -- we need bosh
-        modules_enabled = {
-            "bosh";
-            "pubsub";
-            "ping"; -- Enable mod_ping
-        }
+
+    authentication = "anonymous"
+    -- Properties below are modified by jitsi-meet-tokens package config
+    -- and authentication above is switched to "token"
+    --app_id="app_id"
+    --app_secret="app_secret"
+
+    ssl = {
+        key = "/etc/prosody/certs/localhost.key";
+        certificate = "/etc/prosody/certs/localhost.crt";
+    }
+
+    modules_enabled = {
+        "bosh";
+        "pubsub";
+        "ping";
+    }
 
 Component "conference.{{ key "config/hostname" }}" "muc"
-    --modules_enabled = { "token_verification" }
+    modules_enabled = {
+        --"token_verification"
+    }
 
 VirtualHost "auth.{{ key "config/hostname" }}"
     authentication = "internal_plain"
 
 Component "{{ key "component/focus/auth/user" }}.{{ key "config/hostname" }}"
     component_secret = "{{ key "component/focus/secret" }}"
-
-Component "jitsi-videobridge.{{ key "config/hostname" }}"
-    component_secret = "uAgHm0Z2"
